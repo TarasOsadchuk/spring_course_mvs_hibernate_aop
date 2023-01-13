b74149dfe264a2a4863e5da74be502979556b457
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-13 15:24:00 +0200

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,33 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.dao;
+
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional // анотація вказує, що Spring бере на себе відк/закр транзакції
+    public List<Employee> getAllEmployees() {
+
+        Session session = sessionFactory.getCurrentSession();
+//        List<Employee> allEmployees = session.createQuery("from Employee"
+//                        , Employee.class).getResultList();
+
+        Query<Employee> query = session.createQuery("from Employee"
+                , Employee.class);
+        List<Employee> allEmployee = query.getResultList();
+
+        return allEmployee;
+    }
+}
