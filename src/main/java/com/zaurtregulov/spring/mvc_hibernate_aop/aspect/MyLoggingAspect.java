d43488d83bdceb41520d5f022c2d3c3fe1380fc9
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-14 16:19:32 +0200

    Add AOP

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,29 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    @Around("execution(* com.zaurtregulov.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRRepositoryMethodsAdvice(
+            ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature =
+                (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Begin of " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+
+        return targetMethodResult;
+    }
+}
